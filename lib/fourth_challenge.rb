1141b4d8187df9ee07ee0719b62c4427dfd59e8b
Yahya  Gok <ygok17@hotmail.com>
2020-01-23 01:07:42 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,8 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [  { :name => "Steven", :age => "30", :attitude => "confused"} ,
-      { :name => "Nurse", :age => "44" , :attitude => "worried"}
+      :hero_friends => [  
         #your hashes here!
       ]
    }, 
@@ -13,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [ {
+      :heroine_friends => [ 
         #your hashes here!
       ]
    }
